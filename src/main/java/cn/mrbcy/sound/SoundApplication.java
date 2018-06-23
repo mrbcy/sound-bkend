afab42ae587de16ca9fc7e5601c4de00f5decba5
mrbcy <yang@ricearmy.com>
2018-06-23 11:59:19 +0800

    初始化项目
    springboot自动生成

---
+++ src/main/java/cn/mrbcy/sound/SoundApplication.java
@@ -0,0 +1,12 @@
+package cn.mrbcy.sound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundApplication.class, args);
+	}
+}
